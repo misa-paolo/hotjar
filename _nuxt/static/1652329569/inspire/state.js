65cb027317a5059c49dc1c22cd986ac19b0e5f21
misa-paolo <99472498+misa-paolo@users.noreply.github.com>
2022-05-12 12:28:44 +0800

    initial
    initial

---
+++ _nuxt/static/1652329569/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhotjar\u002F_nuxt\u002Fstatic\u002F1652329569",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Fhotjar\u002F",assetsPath:"\u002Fhotjar\u002F_nuxt\u002F",cdnURL:a}}}}(null));
